5b817522c2c04d94ded2ef7eaab1eea35a607cc5
ESET0090 <gaurav.sonawane@esyasoft.com>
2025-11-06 17:20:27 +0530

    Day 24 Last Commit

--- Day_23 FastApi/app.py
+++ Day_23 FastApi/app.py
@@ -53,6 +53,6 @@ def predict(request: Request,
          "sepal_width": sepal_width,
          "petal_length": petal_length,
          "petal_width": petal_width})
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
